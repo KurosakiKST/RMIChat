a140b7512871d0d6e73556ae016aea447fb9626f
KurosakiKST <kyawcthumoeoo@gmail.com>
2020-02-27 09:48:06 +0630

    Added GUI for chat

--- ClientGUI.java
+++ ClientGUI.java
@@ -1,7 +1,52 @@
 import java.io.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.rmi.Naming;
+import java.util.*;
+
 
 public class ClientGUI {
     public static void main(String[] args) {
-        
+        System.out.println("Started GUI");
+        new ClientGUI();
     }
+    public ClientGUI(){
+        frame=new JFrame("Group Chat");
+	    JPanel main =new JPanel();
+	    JPanel top =new JPanel();
+	    JPanel cn =new JPanel();
+	    JPanel bottom =new JPanel();
+	    ip=new JTextField();
+	    tf=new JTextField();
+	    name=new JTextField();
+	    tx=new JTextArea();
+	    connect=new JButton("Connect");
+	    JButton bt=new JButton("Send");  
+	    main.setLayout(new BorderLayout(5,5));         
+	    top.setLayout(new GridLayout(1,0,5,5));   
+	    cn.setLayout(new BorderLayout(5,5));
+	    bottom.setLayout(new BorderLayout(5,5));
+	    top.add(new JLabel("Your name: "));top.add(name);    
+	    top.add(new JLabel("Your password: "));top.add(ip);
+	    top.add(connect);
+	    cn.add(new JScrollPane(tx), BorderLayout.CENTER);       
+	    bottom.add(tf, BorderLayout.CENTER);    
+	    bottom.add(bt, BorderLayout.EAST);
+	    main.add(top, BorderLayout.NORTH);
+	    main.add(cn, BorderLayout.CENTER);
+	    main.add(bottom, BorderLayout.SOUTH);
+	    main.setBorder(new EmptyBorder(10, 10, 10, 10) );
+	    
+	    frame.setContentPane(main);
+	    frame.setSize(600,600);
+	    frame.setVisible(true);  
+	}
+	JTextArea tx;
+	JTextField tf,ip, name;
+	JButton connect;
+	JFrame frame;
+
 }
